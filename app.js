ccfd36b256f31d42a425afcdeb7531735119be52
Md Ziaur Rahman <iziaurofficial@gmail.com>
2025-12-02 20:39:51 +0530

    Removed app listen

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const path=require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.listen(8080,(req,res)=>{
-    console.log("server is listening to port 8080");
-    
-})
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, "public", "main.html"));
 
